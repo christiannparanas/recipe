12752fd09fff6e5a225d38c2c80b361f0de1390d
Christian Paranas <christiannparanas@gmail.com>
2020-10-18 02:06:06 +0800

    cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,14 +8,8 @@ import Loader from "../components/Loader";
 
 export default function Home() {
   const [recipes, setRecipes] = useState([]);
-  const [ search, setSearch ] = useState("adobo")
-  
-
+  const [search, setSearch] = useState("adobo");
   useEffect(() => {
-    if(!search) {
-      setSearch('adobo')
-    }
-
     const fetchData = async () => {
       const result = await axios.get("https://api.edamam.com/search", {
         params: {
@@ -23,16 +17,25 @@ export default function Home() {
           app_id: "2381de63",
           app_key: "4a9cc5a4fe8d6ae0a61a2b3857997b96",
           form: 0,
-          to: 10,
+          to: 5,
         },
       });
       setRecipes(result.data.hits);
     };
 
     fetchData();
-  }, [search]);
+    if (search === null) {
+      setSearch("adobo");
+    }
+  }, []);
 
-  console.log(recipes);
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      fetchData();
+    }
+  };
 
   return (
     <div className="main">
@@ -50,7 +53,8 @@ export default function Home() {
           <input
             type="text"
             placeholder="Find a Recipe"
-            onInput={(e) => setSearch(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={handleKeypress}
           />
           <div className="fav">
             <FcLike />
@@ -58,6 +62,7 @@ export default function Home() {
         </div>
       </div>
       <Loader />
+      <div className="disc"><div className="dis">Most Popular {search}  recipes!</div></div>
       <div className="items">
         {recipes.map((recipe) => {
           return (
